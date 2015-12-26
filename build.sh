2a0999c142228e39c59354d2aea2c0ba35b53c31
Karl Bowden <karl@karlbowden.com>
2015-12-26 22:58:22 +1100

    Add gitbook build config
    Signed-off-by: Karl Bowden <karl@karlbowden.com>

--- build.sh
+++ build.sh
@@ -1,2 +1,19 @@
-git clone --depth 1 --branch master https://github.com/ReduxKit/ReduxKit.git ReduxKit
-jazzy --config .jazzy.json
+#!/bin/bash
+
+# ----- Clone
+if [ -d ReduxKit ]; then
+  git -C ReduxKit pull origin master
+else
+  git clone --depth 1 --branch master https://github.com/ReduxKit/ReduxKit.git ReduxKit
+fi
+
+# ----- Jazzy
+VERSION=`grep "s.version" ReduxKit/ReduxKit.podspec | cut -d '"' -f2`
+jazzy --config .jazzy.json --module-version "$VERSION"
+
+# ----- Gitbook
+gitbook install
+gitbook build ReduxKit tmp
+rm -rf gitbook
+mv tmp/{*.html,*.json,gitbook} ./
+rm -rf tmp
